cb1f9bdc387f8696b1e6439920e473820f863ca1
Nishkarsh Sharma <nishkarshsharma2004@gmail.com>
2025-07-24 17:32:32 +0530

    add bookmark-button and place in UI

--- Starter Files/content.js
+++ Starter Files/content.js
@@ -6,9 +6,26 @@ function addBookmarkButton(){
     const bookmarkButton = document.createElement('img');
     bookmarkButton.id = "add-bookmark-button";
     bookmarkButton.src = bookmarkImgURL;
+    bookmarkButton.style.cursor = 'pointer';
     bookmarkButton.style.height = "30px";
     bookmarkButton.style.width = "30px";
-
+    bookmarkButton.style.marginLeft = '10px';
+    bookmarkButton.style.marginTop = '3px';
+    bookmarkButton.style.transition = "transform 0.2s ease, filter 0.2s ease";
+    
+    bookmarkButton.addEventListener("mouseover", () => {
+    bookmarkButton.style.transform = "scale(1.1)";
+    bookmarkButton.style.filter = "brightness(1.2)";
+    });
+    
+    bookmarkButton.addEventListener("mouseout", () => {
+      bookmarkButton.style.transform = "scale(1)";
+      bookmarkButton.style.filter = "brightness(1)";
+    });
+    
     const askDoubtButton = document.getElementsByClassName("d-flex flex-row gap-2 justify-content-between m-0 hide-scrollbar ")[0];
     askDoubtButton.insertAdjacentElement("afterend", bookmarkButton);
+    bookmarkButton.addEventListener("click",()=>{
+        alert("Your Problem has been stored");
+    })
 }
